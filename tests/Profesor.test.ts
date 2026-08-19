5c6eebb9ad13e58521f374af7d82bc3209842c3f
Jose A. Fernandez <fernandezja@gmail.com>
2026-08-19 17:30:25 -0300

    Fix test Materia y Profesor

--- tests/Profesor.test.ts
+++ tests/Profesor.test.ts
@@ -1,14 +1,14 @@
 import { describe, expect, test } from "vitest";
-import { Alumno } from "../src/Profesor";
+import { Profesor } from "../src/Profesor";
 
 describe("Profesor", () => {
-
-
+ 
   test("Un profesor debe tener un nombre", () => {
     
     var profesor1 = new Profesor('Profesor 1');
 
     expect(profesor1.nombre).toBe('Profesor 1');
   });
+  
 
 });
